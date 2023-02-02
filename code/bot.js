efff65668303c84b504ae990def928d020889280
Vasilii <chapaevpustota@gmail.com>
2023-02-02 18:37:07 +0300

    wake_up

--- code/bot.js
+++ code/bot.js
@@ -1,7 +1,7 @@
 //const config = require("../config.json");
-//const index_url = config.index_url;
+const index_url = process.env.index_url;
 
-//const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 //const TwitchBot = require("./twitch_bot.js");
 const DiscordBot = require("./discord_bot.js");
 const server = require("./server");
@@ -11,12 +11,12 @@ class Bot {
         this.server = server(this);
         this.discord_bot = new DiscordBot(this);
 //        this.twitch_bot = new TwitchBot(this);
- //       this.wake_up();
+        this.wake_up();
     }
 
     wake_up() {
         fetch(index_url);
-        setTimeout(() => this.wake_up(), 1000 * 60 * 20);
+        setTimeout(() => this.wake_up(), 1000 * 60 * 10);
     }
 
 };
